078e8034fd2f9277dc57179ecf653672fcc1d1f5
Axel Nuñez <aranuo23@gmail.com>
2024-01-18 22:57:05 -0300

    Agregando productosDisponibles y carrito (Objetos). Y documentando proyecto

---
+++ js/funcion Order Superior.js
@@ -0,0 +1,40 @@
+// Distintos metodos para recorrer, modificar, ordenar Objetos
+const productoDisponible = [
+    { nombre: "zapas axel", categoria: "zapatillas", precio: 70000 },
+    { nombre: "nike", categoria: "zapatillas", precio: 120000 },
+    { nombre: "adidas", categoria: "zapatillas", precio: 50000 },
+    { nombre: "camiseta", categoria: "remeras", precio: 20000 },
+    { nombre: "short", categoria: "pantalones", precio: 180000 }
+];
+
+
+// el forEach sirve para recorrer objeto/array
+productoDisponible.forEach( (item) => {
+    console.log(item.nombre);
+    alert("Actualmente están disponibles los siguientes productos: (" + item.nombre + ")");
+
+} )
+
+
+//el find sirve para encontrar elementos del array/objeto por medio de comparación 
+const categorias = productoDisponible.find( (filtrar) => filtrar.nombre == ("nike")) 
+console.log(categorias);
+alert("Ahora vamos a filtrar por categoría zapatillas usando find: " + categorias);
+
+
+// el filter funciona como un find que sirve para buscar elementos y si existe lo retorna. 
+categoriasFilter = productoDisponible.filter( (filtrar) => filtrar.categoria.includes ("zapa") ) 
+console.log(categoriasFilter);
+alert("Ahora vamos a filtrar por categoría zapatillas usando filter: " + categoriasFilter);
+
+
+
+// el metodo some recibe una funcion que retorna true o false segun la busqueda
+const encontrarCategoriaLog = productoDisponible.some ((encontrarCategoria) => encontrarCategoria.categoria == "remeras")
+console.log(encontrarCategoriaLog);
+
+// el metodo map sirve para crear un nuevo array con los elementos del array original o objeto que teníamos, según la operacion 
+//que le indiquemos por parametro 
+const mostrarNombres = productoDisponible.map((arrayNombres) => arrayNombres.nombre )
+console.log(mostrarNombres)
+alert("Ahora podemos ver los nombres del objeto " + mostrarNombres)
--- js/funciones.js
+++ js/funciones.js
@@ -40,28 +40,57 @@ let contrasenia = login();
 
 
 
-//Array para agregar productos al carrito
-const carrito = [];
-let producto;
+//Objeto de productos disponibles para luego agregar al carrito
+const productoDisponible = [
+    { nombre: "zapas axel", categoria: "zapatillas", precio: 70000 },
+    { nombre: "nike", categoria: "zapatillas", precio: 120000 },
+    { nombre: "adidas", categoria: "zapatillas", precio: 50000 },
+    { nombre: "camiseta", categoria: "remeras", precio: 20000 },
+    { nombre: "short", categoria: "pantalones", precio: 180000 }
+];
 
-while (confirm ("¿Desea agregar productos?") == true){
-    producto = prompt("Escribir un producto: ");
-    carrito.push (producto);
-    console.log(producto);
+
+// Objeto del carrito donde se van a agregar los productos
+let carritoProductos = []
+
+
+
+//Recorrer objeto
+for(let i = 0; i<productoDisponible.length; i++){
+    console.log("nombre: " + productoDisponible[i].nombre);
+    console.log("precio: " + productoDisponible[i].precio);
 }
 
-alert("Tenes estos productos en el carrito: " + carrito );
+//Utilizo metodo map para mostrar los productos que están disponibles
+const mostrarNombres = productoDisponible.map((arrayNombres) => arrayNombres.nombre )
+console.log(mostrarNombres)
+alert("Estos son los productos disponibles: " + mostrarNombres)
 
 
-// Sacar productos del carrito
-while(confirm("¿Desea sacar algún producto?") == true){
+// Bucle para agregar productos disponibles al carrito
+while (confirm ("¿Desea agregar al carrito algún producto disponible?") == true){
+    let agregarCarritoProductos = prompt("Favor elegir algún producto disponible: ");
+    let posicionProductos = carritoProductos.indexOf(agregarCarritoProductos);
+    productoDisponible.splice (posicionProductos,1);
+    carritoProductos.push (agregarCarritoProductos)
+    console.log(productoDisponible);
+    console.log(carritoProductos);
+}
+
+alert("Tenes estos productos en el carrito: " + carritoProductos );
+
+
+
+// Bucle para eliminar productos del carrito
+while(confirm("¿Desea eliminar algún producto?") == true && carritoProductos != ""){
     let sacarProducto = prompt("Elegir el producto que desea sacar");
-    let pocisionProducto = carrito.indexOf(sacarProducto);
-    carrito.splice(pocisionProducto,1);
-    console.log(carrito);
+    let pocisionProducto = carritoProductos.indexOf(sacarProducto);
+    carritoProductos.splice(pocisionProducto,1);
+    console.log(carritoProductos);
 }
-
-alert("Tenes estos productos en el carrito: " + carrito );
+if(carritoProductos == ""){
+alert("Ya no te quedan productos en el carrito que puedas eliminar😪");
+}
 
 let monto = parseFloat(prompt("Ingrese el monto que desea pagar en cuotas"));
 let resultado = pagoCuotas(monto);
